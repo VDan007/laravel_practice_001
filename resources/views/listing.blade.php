2cb9d1c9de0d1e723a5c8a89427a1cd0133b6f78
DanielVarjaskéri <varjaskerid@gmail.com>
2024-01-14 10:24:53 +0100

    next steps of learning

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,3 +1,8 @@
+@extends('layout')
+@section('content')
+
 <h1>Listing</h1>
 <h2>{{$listing['id']}}</h2>
 <p>{{$listing['description']}}</p>
+
+@endsection
--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,5 +1,13 @@
+
+@extends('layout')
+
+@section('content')
+    
+
 <h1>Listings</h1>
 @foreach($listings as $listing)
     <h1> <a href="listing/{{$listing['id']}}"> {{$listing['title']}} </a> </h1>
     <p> {{$listing['description']}} </p>
 @endforeach
+
+@endsection
